79432dd666aa3743895a3d19b5e0a576a8a3b5d2
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-09-09 01:51:36 +0100

    Feat:adding the login page for testing ans connecting database + setting up the routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const apiRoutes = require("./routes/index");
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.get("/", (req, res) => {
+  res.send("Hello World ha!");
 });
+
+app.get("/login", (req, res) => {
+  res.render("pages/login"); 
+});
+
+app.get("/test", (req, res) => {
+  res.send("Test route is working");
+});
+
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
