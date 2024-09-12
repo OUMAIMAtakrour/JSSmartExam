8c17c091bfcb082e3a2da6b8cc94dd18a8a3825f
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-09-12 10:09:17 +0100

    Fix:fixed the auth routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+const authRoutes = require("./routes/authRoutes");
+
 const subjectRoutes = require("./routes/subjectRoutes");
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +15,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "..", "views"));
 
 app.use(express.static(path.join(__dirname, "../public")));
-
+app.use("/auth", authRoutes);
 app.get("/login", (req, res) => {
   res.render("pages/login");
 });
